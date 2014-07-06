2217345871036f9bfb87d060a0f14c9b1a8c5fb3
Clinton Andrews <candrews800@gmail.com>
2014-07-06 17:55:46 -0400

    Update remember_token

---
+++ app/database/migrations/2014_07_06_215248_update_remembertoken_to_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateRemembertokenToUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+            $table->drop('remember_token');
+            $table->
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
