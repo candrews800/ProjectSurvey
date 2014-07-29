cf7369751db81d7737c49bb192771a69b27def02
Clinton Andrews <candrews800@gmail.com>
2014-07-28 20:56:13 -0500

    UI Additions to Password Reminder

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -1,4 +1,29 @@
-<form action="{{ action('RemindersController@postRemind') }}" method="POST">
-    <input type="email" name="email">
-    <input type="submit" value="Send Reminder">
-</form>
+@if (isset($status))
+{{ $status }}
+@endif
+
+@if (isset($error))
+{{ $error }}
+@endif
+
+@include('layouts.header')
+
+<div class="row">
+    <div class="small-12 medium-6 columns">
+        {{ Form::open(array('action' => 'RemindersController@postRemind')) }}
+        <fieldset>
+            <legend>Forgot Your Password?</legend>
+
+            {{ Form::label('email', 'Enter Your Email Address') }}
+            {{ Form::email('email', null, array('placeholder' => 'Email', 'required' => 'required')) }}
+            @if ($errors->has('email'))
+                <small class="error">{{ $errors->first('email') }}</small>
+            @endif
+
+            {{ Form::submit('Send Reminder', array('class' => 'button')) }}
+        </fieldset>
+        {{ Form::close() }}
+    </div>
+</div>
+
+@include('layouts.footer')
--- app/views/password/reset.blade.php
+++ app/views/password/reset.blade.php
@@ -1,7 +1,46 @@
-<form action="{{ action('RemindersController@postReset') }}" method="POST">
-    <input type="hidden" name="token" value="{{ $token }}">
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <input type="password" name="password_confirmation">
-    <input type="submit" value="Reset Password">
-</form>
+@include('layouts.header')
+
+<div class="row">
+    <div class="small-12 medium-6 columns">
+        {{ Form::open(array('action' => 'RemindersController@postReset')) }}
+        <input type="hidden" name="token" value="{{ $token }}">
+        <fieldset>
+            <legend>Forgot Your Password?</legend>
+
+            <div class="row">
+                <div class="medium-12 columns">
+                    {{ Form::label('email', 'Email') }}
+                    {{ Form::email('email', null, array('placeholder' => 'Email', 'required' => 'required')) }}
+                    @if ($errors->has('email'))
+                        <small class="error">{{ $errors->first('email') }}</small>
+                    @endif
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="medium-12 columns">
+                    {{ Form::label('password', 'Password') }}
+                    {{ Form::password('password', array('placeholder' => 'Password', 'required' => 'required')) }}
+                    @if ($errors->has('password'))
+                    <small class="error">{{ $errors->first('password') }}</small>
+                    @endif
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="medium-12 columns">
+                    {{ Form::label('password_confirmation', 'Confirm Password') }}
+                    {{ Form::password('password_confirmation', array('placeholder' => 'Password', 'required' => 'required')) }}
+                    @if ($errors->has('password_confirmation'))
+                        <small class="error">{{ $errors->first('password_confirmation') }}</small>
+                    @endif
+                </div>
+            </div>
+
+            {{ Form::submit('Reset Password', array('class' => 'button')) }}
+        </fieldset>
+        {{ Form::close() }}
+    </div>
+</div>
+
+@include('layouts.footer')
